949fbb5cb3365ada7a4c13555220187883e729be
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-18 03:23:54 +0400

    update

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        toBeDivided = scanner.nextInt();
+        toBeDivided = 10;
 
         int result = toBeDivided / 5;
 
         System.out.println( result);
-
-        scanner.close();
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class TimeConvertor {
     public void convert(float minutes) {
 
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of minutes: ");
-        minutes = scanner.nextInt();
+        minutes = 5;
 
         float seconds = minutes * 60; // Convert minutes to seconds
 
         System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
 
-        scanner.close();
     }
 }
