4acd813da794dd395090323780b00f3d61f75239
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-18 03:19:37 +0400

    update

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,19 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of minutes: ");
+        minutes = scanner.nextInt();
+
+        float seconds = minutes * 60; // Convert minutes to seconds
+
+        System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+
+        scanner.close();
     }
 }
