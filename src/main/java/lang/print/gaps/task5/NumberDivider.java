2b504595249ae91136fb8d3f9a8073e474b28a57
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-18 03:51:42 +0400

    update

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,12 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int result = toBeDivided / 5;
+        double result = (double) toBeDivided / 5;
         System.out.println( result);
-    }
-    public static void main(String[] args) {
-        NumberDivider divider = new NumberDivider();
-        int numberToBeDivided = 19;
-        divider.divide(numberToBeDivided);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -6,12 +6,12 @@ public class TimeConvertor {
     public void convert(float minutes) {
 
         float seconds = minutes * 60.0f; // Convert minutes to seconds
-        System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+        System.out.println( seconds );
     }
 
     public static void main(String[] args) {
         TimeConvertor convertor = new TimeConvertor();
-        float minutesToConvert = 2.5f; // You can change this value
+        float minutesToConvert = 7.5f;
         convertor.convert(minutesToConvert);
     }
 }
