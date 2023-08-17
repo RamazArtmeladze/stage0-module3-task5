9b64260faed9b1cb27adf7a9641c9e9cf8b770c8
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-18 03:41:10 +0400

    update

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,14 +1,8 @@
 package lang.print.gaps.task5;
 
-import java.util.Scanner;
-
 public class NumberDivider {
     void divide(int toBeDivided) {
-
-        toBeDivided = 10;
-
         int result = toBeDivided / 5;
-
         System.out.println( result);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class TimeConvertor {
     public void convert(float minutes) {
 
-
         minutes = 5;
 
         float seconds = minutes * 60; // Convert minutes to seconds
 
-        System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+        System.out.println( seconds );
 
     }
 }
